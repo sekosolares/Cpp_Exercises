a1e2d5427b8e840d67b06f1de04743cb9aae8460
sekosolares <axel.solares.g@gmail.com>
2021-01-04 01:59:19 -0600

    Update: Reading information from created file.

--- files.cpp
+++ files.cpp
@@ -4,17 +4,36 @@
 using namespace std;
 
 int main() {
-
+	/*
 	ofstream MyFile("GameData.txt");
+	string PlayerName = "";
 
 	if ( MyFile.is_open() ) {
 		MyFile << "Hola, Amigo!" << endl;
-		for (int i = 0; i < 10; ++i) {
-			MyFile << i << endl;
-		}
+		MyFile << "Mi nombre es: " << endl;
+		cout << "Introduce el nombre de tu heroe: ";
+		cin >> PlayerName;
+		MyFile << PlayerName;
 	}
 
 	MyFile.close();
+	*/
+	ifstream  MyFileRead("GameData.txt");
+	string line, NombreHeroe = "";
+	int Renglon = 0;
+
+	if ( MyFileRead.is_open() ) {
+		while( getline(MyFileRead, line) ) {
+			if ( Renglon == 2 )
+				NombreHeroe = line;
+			++Renglon;
+		}
+	} else {
+		cout << "No se logro abrir el archivo! :(" << endl;
+	}
+
+	cout << "Bienvenida/o a tu aventura, " << NombreHeroe << "!" << endl;
+	cout << "Te estabamos esperando!" << endl;
 
 	return 0;
 }
